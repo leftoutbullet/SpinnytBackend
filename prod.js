b0ba3c38f126d8d47c7218776e6a79f6bff356da
Mohamed Khalil Boubaker <khalil.boubaker@etudiant-isi.utm.tn>
2024-09-12 13:21:07 +0100

    yet another one3

--- prod.js
+++ prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.port,
+    port: process.env.PORT || 13567,
     mongoURI: process.env.MONGO_URI,
 
 };
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 
 //Setting up DB
 const mongoose = require('mongoose');
+console.log("MongoURI:", keys.mongoURI);
+console.log("Port:", keys.port);
+
 mongoose.connect(keys.mongoURI);
 //setting up DB models
 require('./model/Score.js');
